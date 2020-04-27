23346f221587be03333c973f0eb69b87d9ced905
Jean-Romain Garnier <git@jean-romain.com>
2020-04-27 22:06:32 +0200

    Create framework project

---
+++ MangaDexLib/MangaDexLib.h
@@ -0,0 +1,19 @@
+//
+//  MangaDexLib.h
+//  MangaDexLib
+//
+//  Created by Jean-Romain on 27/04/2020.
+//  Copyright © 2020 JustKodding. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MangaDexLib.
+FOUNDATION_EXPORT double MangaDexLibVersionNumber;
+
+//! Project version string for MangaDexLib.
+FOUNDATION_EXPORT const unsigned char MangaDexLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MangaDexLib/PublicHeader.h>
+
+
