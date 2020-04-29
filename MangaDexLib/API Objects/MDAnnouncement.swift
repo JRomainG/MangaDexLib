72bde50e38099e0bea272a2bcc7a9c603cc56049
Jean-Romain Garnier <git@jean-romain.com>
2020-04-29 02:00:04 +0200

    Allow parser to extract announcements and alerts

---
+++ MangaDexLib/API Objects/MDAnnouncement.swift
@@ -0,0 +1,23 @@
+//
+//  MDAnnouncement.swift
+//  MangaDexLib
+//
+//  Created by Jean-Romain on 29/04/2020.
+//  Copyright © 2020 JustKodding. All rights reserved.
+//
+
+import Foundation
+
+/// Class representing an announcement returned by MangaDex
+struct MDAnnouncement {
+
+    /// The content of the announcement
+    ///
+    /// The body may still contain HTML elements, like
+    /// `<a>`, `<br>`, `<strong>`...
+    var body: String?
+
+    /// The content of the announcement, stripped of its HTML elements
+    var textBody: String?
+
+}
--- MangaDexLibTests/MDLibParserTests.swift
+++ MangaDexLibTests/MDLibParserTests.swift
@@ -19,6 +19,40 @@ class MDLibParserTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
+    func testGetAnnouncement() throws {
+        let url = URL(string: MDApi.baseURL)!
+        let requestHandler = MDRequestHandler()
+        let parser = MDParser()
+        let expectation = self.expectation(description: "Load MangaDex's homepage")
+
+        requestHandler.get(url: url) { (content, _) in
+            guard let announcement = parser.getAnnouncement(from: content!) else {
+                return
+            }
+            XCTAssertNotNil(announcement.body)
+            XCTAssertNotNil(announcement.textBody)
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 15, handler: nil)
+    }
+
+    func testGetAlerts() throws {
+        let url = URL(string: "\(MDApi.baseURL)/login")!
+        let requestHandler = MDRequestHandler()
+        let parser = MDParser()
+        let expectation = self.expectation(description: "Load MangaDex's login page")
+
+        requestHandler.get(url: url) { (content, _) in
+            let alerts = parser.getAlerts(from: content!)
+            for alert in alerts {
+                XCTAssertNotNil(alert.body)
+                XCTAssertNotNil(alert.textBody)
+            }
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 15, handler: nil)
+    }
+
     func testLoginCheck() throws {
         let url = URL(string: "\(MDApi.baseURL)/search?title=title")!
         let requestHandler = MDRequestHandler()
